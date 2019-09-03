6482d9c8d63dced01921b2f3f9112264748a5764
IzoldaArchel <kolomiets1983@mail.ru>
2019-09-03 02:16:59 -0400

    Uploaded files
    Project files

---
+++ Migrations/20190703092732_SeedDataUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class SeedDataUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 2, "john@pragimtech.com", "John" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "employees",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
---
+++ Models/SQLEmployeeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class SQLEmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext context;
+        public SQLEmployeeRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public Employee Add(Employee employee)
+        {
+            context.employees.Add(employee);
+            context.SaveChanges();
+            return employee;
+        }
+
+        public Employee Delete(int id)
+        {
+            Employee employee = context.employees.Find(id);
+            if (employee != null)
+            {
+                context.employees.Remove(employee);
+                context.SaveChanges();
+            }
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return context.employees;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return context.employees.Find(Id);
+        }
+
+        public Employee Update(Employee employeeChanges)
+        {
+            var employee = context.employees.Attach(employeeChanges);
+            employee.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+            return employeeChanges;
+        }
+    }
+}
